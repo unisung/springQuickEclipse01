c340a6345d969b0cab771ba23fe1876071a475da
unisung <vctor@naver.com>
2021-04-20 11:54:44 +0900

    04-20-001.spring-security-접근제한 설정 전.

---
+++ ex06/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,31 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+  @GetMapping("/all") //  /sample/all - 모든 유저 접속 가능 
+  public void doAll() {
+	  log.info("do all can access everybody");
+  }
+  
+  @GetMapping("/member")  //  /sample/member- 회원만 접근 가능 리소스
+  public void doMember() {
+	  log.info("logined member");
+  }
+  
+  @GetMapping("/admin") // /sample/admin - 관리자만 접근 가능 리소스
+  public void doAdmin() {
+	  log.info("admin only");
+  }
+     
+  
+	
+}
