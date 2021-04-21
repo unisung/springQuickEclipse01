f70a2f7dbc5e05c6735c803a6f562ee5e7a1852a
unisung <vctor@naver.com>
2021-04-21 16:33:51 +0900

    04-21-security-annotations.

--- ex06/src/main/java/org/zerock/controller/SampleController.java
+++ ex06/src/main/java/org/zerock/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.zerock.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,18 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')")		
+  @GetMapping("/annoMember")
+  public void doMember2() {
+	 log.info("logined annotation member");  
+  }
+  
+  @Secured({"ROLE_ADMIN"})
+  @GetMapping("/annoAdmin")
+  public void doAdmin2() {
+	  log.info("admin annotation only");
+  }
 	
   @GetMapping("/all") //  /sample/all - 모든 유저 접속 가능 
   public void doAll() {
