28933dda10fb05a56c29b16ffe5427622f125a23
unisung <vctor@naver.com>
2021-04-22 12:17:22 +0900

    04-22-003. proj01.

--- ex06_2/src/main/java/org/zerock/controller/BoardController.java
+++ ex06_2/src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,14 +58,17 @@ public class BoardController {
 	 * log.info("list"); model.addAttribute("list", service.getList()); return
 	 * "list"; }
 	 */
-	
-	  @GetMapping("/register") // /board/register
+
+	  
+    @GetMapping("/register") // /board/register
+    @PreAuthorize("isAuthenticated()")
 		public void register() {
 		}
 	  
 	  
 	/* 게시글 등록 */
 	@PostMapping("/register") // /board/register
+	@PreAuthorize("isAuthenticated()")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		log.info("=====================");
 		log.info("register: "+board);
--- ex06_2/src/test/java/org/zerock/controller/SampleController_RESTTests.java
+++ ex06_2/src/test/java/org/zerock/controller/SampleController_RESTTests.java
@@ -24,7 +24,7 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml",
 	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 @Log4j
-public class SampleControllerTests {
+public class SampleController_RESTTests {
 
 	  @Setter(onMethod_=@Autowired)
 	  private WebApplicationContext ctx;
